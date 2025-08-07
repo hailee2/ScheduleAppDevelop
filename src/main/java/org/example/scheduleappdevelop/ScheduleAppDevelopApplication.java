e55ca5246f7c34df3afe2b2bacc3ae965302a369
hailee2 <slowslow9150@gmail.com>
2025-08-07 17:19:51 +0900

    Initial commit

---
+++ src/main/java/org/example/scheduleappdevelop/ScheduleAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package org.example.scheduleappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAppDevelopApplication.class, args);
+    }
+
+}
